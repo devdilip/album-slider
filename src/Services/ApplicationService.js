1ec142ce67ba3e8e772bd3b0a103a2673d4b0977
Dilip Kumar <dilip.kumar@zestmoney.in>
2019-09-16 13:50:06 +0530

    Added Application Service for api call

---
+++ src/Services/ApplicationService.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://jsonplaceholder.typicode.com/';
+const EACH_ALBUM_DETEILS_URL = 'photos?albumId=';
+const ALL_ALBUM_DETAILS_URL = 'albums';
+
+
+export const getAllAlbumDetailsService = async () => {
+    try {
+        const response = await axios(getDataOptions(ALL_ALBUM_DETAILS_URL));
+        return response.data;
+    } catch (error) {
+        return Promise.reject(error);
+    }
+};
+
+
+export const updateAlbumDataService = async (id) => {
+    const url = EACH_ALBUM_DETEILS_URL + id;
+    try {
+        const response = await axios(getDataOptions(url));
+        return response.data;
+    } catch (error) {
+        return Promise.reject(error);
+    }
+};
+
+export const getDataOptions = (url) => {
+    const options = {
+        method: 'get',
+        baseURL: BASE_URL,
+        url: url,
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        }
+    }
+    return options;
+};
