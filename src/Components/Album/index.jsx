8cdb38b412d2e93560ceef84122f0dadeb39c4ed
Dilip Kumar <dilip.kumar@zestmoney.in>
2019-09-20 00:03:56 +0530

    Handle network connection

--- src/Components/Album/index.jsx
+++ src/Components/Album/index.jsx
@@ -6,7 +6,8 @@ const AlbumRow = React.lazy(() => import('./../AlbumRow'));
 
 export default class componentName extends Component {
   state = {
-    albumList: []
+    albumList: [],
+    networkError: false
   };
   componentDidMount() {
     this.GetAllAlbumDetails()
@@ -14,19 +15,31 @@ export default class componentName extends Component {
 
   GetAllAlbumDetails = async () => {
     await getAllAlbumDetailsService().then(response => {
-      this.setState({ albumList: response });
+      this.setState({ albumList: response, networkError: false });
     }).catch(error => {
+      this.setState({ networkError: true });
       console.log(error);
     });
+  }
+
+  networkErrorMessage = () => {
+    return (
+      <div className="card m-2">
+        <div className="card-header text-center text-danger">
+          <h5 className="p-1 m-1">Error: Network Connection | Check in console for logs</h5>
+        </div>
+      </div>);
   }
 
   render() {
     return (
       <Suspense fallback={<div> Loading... </div>}>
         <React.Fragment>
-          {this.state.albumList.map((item, i) => (
-            <AlbumRow data={item} key={i} />
-          ))}
+          {this.state.networkError
+            ? this.networkErrorMessage()
+            : this.state.albumList.map((item, i) => (
+              <AlbumRow data={item} key={i} />
+            ))}
         </React.Fragment>
       </Suspense>
     );
